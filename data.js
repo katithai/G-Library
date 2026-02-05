effb0d2be291656fdd2d504efd4d4f784c34a199
katithai <thanhcongthai2005@gmail.com>
2026-02-05 15:30:49 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,74 @@
+const KHO_GAME = [
+    {
+        "id": "fifa_16",
+        "ten": "FIFA 16",
+        "dung_luong": "18.7 GB",  
+        "anh_bia": "https://cdn2.steamgriddb.com/hero_thumb/49c155ea98976cfdedfee27510f7c929.jpg",
+        "icon": "https://cdn2.steamgriddb.com/icon/e046cbb371a979a6893456eee7f00703.png",
+        "mota": [
+            "Cấu hình để chơi game FIFA 16",
+            "OS: Windows V/7/8/8.1 - 64-Bit",
+            "CPU: Intel Q6600 Core2 Quad @ 2.4Ghz (or AMD Phenom 7950 Quad-Core, AMD Athlon II X4 620 equivalent)",
+            "RAM: 4GB",
+            "Hard Drive Space Required: 15.0 GB",
+            "Minimum Supported Video Cards: ATI Radeon HD 5770, NVIDIA GTX 650"
+        ],
+
+        "links": [
+            "https://drive.usercontent.google.com/download?id=1YlRKVPWNiOBNUVtYSfAN0sUvGDpw-zi3&authuser=0",
+            "https://drive.usercontent.google.com/download?id=155iNCT8afRjltKf6yZl6cBobLflZ0xQa&authuser=0",
+            "https://pixeldrain.com/u/GnBhMHXN"
+        ]
+    },
+    {
+        "id": "pes_2016",
+        "ten": "PES 2016",
+        "dung_luong": "4.84 GB",
+        "anh_bia": "https://cdn2.steamgriddb.com/hero_thumb/0b921d8fd554dbac8df556dffc3828a6.jpg",
+        "icon": "https://cdn2.steamgriddb.com/icon_thumb/ca5d884eb0cbbda1dcadc5bc5c8f7aba.png", 
+        "mota":[
+            "BẢN TiNYiSO",
+            "v1.05 + Full DLCs Data Pack 4.0/UEFA."
+        ],
+
+        "links": [
+            "https://drive.usercontent.google.com/download?id=12XPzF3FLg-AWidvu_d_5u0GMmLkHmFaj&authuser=0"
+        ]
+    },
+    {
+        "id": "wallpapere_ngine",
+        "ten": "Wallpaper Engine",
+        "dung_luong": "800M",
+        "anh_bia": "https://cdn2.steamgriddb.com/hero_thumb/07ebae5de4be22e8d3916839a654d94a.jpg",
+        "icon": "https://cdn2.steamgriddb.com/icon_thumb/2611fdd1a2a998be4d3af1466d41d9d1.png", 
+        "mota": [
+            "Wallpaper Engine - Auto Update + Mod Manager | v9",
+            "Cài mod bằng file LND Mod Manager (có thể xem hướng dẫn bằng cách bấm vào nút Tutorial)",
+            "Update phần mềm bằng file _Updater"
+        ],
+        "links": [
+            "https://drive.usercontent.google.com/download?id=1DaxfTgcEOBrfQ8JFSnE52klgs79_Kx2p&authuser=0"
+        ]
+    },
+    {
+        "id": "f1_2013",
+        "ten": "F1 2013",
+        "dung_luong": "10 GB",
+        "anh_bia": "https://cdn2.steamgriddb.com/hero_thumb/222aaac522b686a196dc52372498ac8f.jpg",
+        "icon": "https://cdn2.steamgriddb.com/icon_thumb/957373826a9bebce24d888b6f25cf37e.png", 
+        "mota": [
+            "Pass giải nén file game là: hadoantv.com",
+            "Cấu hình tối thiểu",
+            "Windows Vista, Windows 7 hoặc Windows 8",
+            "Intel Core 2 Duo @ 2.4Ghz hoặc AMD Athlon X2 5400+",
+            "Bộ nhớ: 2G",
+            "AMD HD2600 hoặc NVIDIA Geforce 8600",
+            "Lưu trữ: 10 GB chỗ trống khả dụng"
+        ],
+        "links": [
+            "https://drive.usercontent.google.com/download?id=1ziVO3WjyU9Il4JqRUOZG3F_eQN-YbRw9&export=download&authuser=0"
+        ]
+    }
+
+
+];
