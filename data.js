611ccf433603c1b29703ad64ee3ac766c581ceb9
katithai <thanhcongthai2005@gmail.com>
2026-03-17 09:50:20 +0700

    Update data.js

--- data.js
+++ data.js
@@ -95,7 +95,7 @@ const KHO_GAME = [
         "links": [
             "https://drive.usercontent.google.com/download?id=10gCyUcy-7BKN-GIwpPVzvWaj_j4G1dh6&authuser=0",
             "https://linkneverdie.com/f/c226426b-b620-43de-b9ec-609696ac75b6",
-            "https://drive.usercontent.google.com/download?id=10jUJ58hmZSRgJOv4oFEoaOZXNKJAIiV_&authuser=0",
+            "https://drive.usercontent.google.com/download?id=10jUJ58hmZSRgJOv4oFEoaOZXNKJAIiV_&authuser=0"
         ]
     }
 
